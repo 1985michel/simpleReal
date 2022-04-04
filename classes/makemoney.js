62f4c473ba825610e489a74d038a59ece4a64036
T-Gamer <1985michel>
2022-04-04 10:30:44 -0300

    criada classe MakeMoney referente a recebimentos de rendas ativas e passivas

---
+++ classes/makemoney.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class MakeMoney {
+    constructor(isRendaAtiva, origem, valor, contaDeRecebimento, dataRecebimento) {
+
+        this.isRendaAtiva = isRendaAtiva;
+        this.origem = origem;
+        this.valor = valor;
+        this.contaDeRecebimento = contaDeRecebimento;
+        this.dataRecebimento = dataRecebimento;
+
+    }
+}
