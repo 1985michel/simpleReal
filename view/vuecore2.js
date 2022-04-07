dcb72e62c6970e3262a75db13325c2aed247124a
T-Gamer <1985michel>
2022-04-07 19:32:19 -0300

    conseguimos fazer o download do backup

--- view/vuecore2.js
+++ view/vuecore2.js
@@ -1635,8 +1635,25 @@ const vm = new Vue({
 
 
 
-        }
+        },
 
+
+        downloadBackup() {
+
+            ativaStringfyInOut(this.contas, this.compromissos, this.compromissosDoMes, this.recebimentos);
+            const content = document.getElementById("txtarea2").value;
+            //const content = (stringfyAll(contas, compromissos, compromissosDoMes, recebimentos)).toString();
+            const mimeType = 'text/plain';
+            const filename = 'backup.txt';
+
+
+            const a = document.createElement('a') // Create "a" element
+            const blob = new Blob([content], { type: mimeType }) // Create a blob (file-like object)
+            const url = URL.createObjectURL(blob) // Create an object URL from blob
+            a.setAttribute('href', url) // Set "a" element link
+            a.setAttribute('download', filename) // Set download filename
+            a.click() // Start downloading
+        }
 
 
 
